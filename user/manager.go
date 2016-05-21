4e4964163637a3a6d9ebff8bc0a4273e389efa17
kyorohiro <kyorohiro@gmail.com>
2016-05-22 03:43:03 +0900

    ..

--- user/manager.go
+++ user/manager.go
@@ -5,11 +5,6 @@ import (
 
 	"google.golang.org/appengine/datastore"
 
-	"crypto/rand"
-	"encoding/binary"
-	"encoding/json"
-	"strconv"
-
 	"time"
 
 	"golang.org/x/net/context"
@@ -72,34 +67,6 @@ func (obj *UserManager) GetUserFromMail(ctx context.Context, mail string) (*User
 func (obj *UserManager) RegistUser(ctx context.Context, userName string, passIdFromClient string, email string) (*User, error) {
 	user := obj.NewUser(ctx, userName)
 	return user, user.Regist(ctx, passIdFromClient, email)
-}
-
-//
-// And Regist LoginID
-//
-func (obj *UserManager) RegistUserFromTwitter(ctx context.Context, screenName string, userId string, oauthToken string, oauthSecret string) (*User, error) {
-	user := obj.NewUser(ctx, screenName+"@twitter")
-	dummyPass := screenName + obj.MakeRandomId() + obj.MakeRandomId()
-	return user, user.Regist(ctx, dummyPass, "")
-}
-
-func (obj *UserManager) LoginUserFromTwitter(ctx context.Context, //
-	screenName string, userId string, oauthToken string, oauthSecret string, //
-	remoteAddr string, userAgent string) (string, *User, error) {
-	userObj, err := obj.GetFromUserName(ctx, screenName+"@twitter")
-	if err != nil {
-		return "", nil, ErrorNotFound
-	}
-	//
-	//
-	m := map[string]interface{}{"oauth_token": oauthToken, "oauth_token_secret": oauthSecret, "screen_name ": screenName, "user_id": userId}
-	b, _ := json.Marshal(m)
-	//
-	loginIdObj, err1 := obj.NewAccessToken(ctx, screenName+"@twitter", remoteAddr, userAgent, string(b))
-	if err1 == nil {
-		obj.UpdateMemcache(ctx, loginIdObj)
-	}
-	return loginIdObj.gaeObject.LoginId, userObj, err1
 }
 
 func (obj *UserManager) LoginUser(ctx context.Context, userName string, passIdFromClient string, remoteAddr string, userAgent string) (string, *User, error) {
@@ -166,10 +133,4 @@ func (obj *UserManager) DeleteUser(ctx context.Context, userName string, passIdF
 	user.gaeObject.Status = UserStatusDelete
 	err = user.PushToDB(ctx)
 	return err
-}
-
-func (obj *UserManager) MakeRandomId() string {
-	var n uint64
-	binary.Read(rand.Reader, binary.LittleEndian, &n)
-	return strconv.FormatUint(n, 36)
 }
---
+++ user/manager_twitter.go
@@ -0,0 +1,44 @@
+package gaeuser
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+	"encoding/json"
+	"strconv"
+
+	"golang.org/x/net/context"
+)
+
+//
+// And Regist LoginID
+//
+func (obj *UserManager) RegistUserFromTwitter(ctx context.Context, screenName string, userId string, oauthToken string, oauthSecret string) (*User, error) {
+	user := obj.NewUser(ctx, screenName+"@twitter")
+	dummyPass := screenName + obj.MakeRandomId() + obj.MakeRandomId()
+	return user, user.Regist(ctx, dummyPass, "")
+}
+
+func (obj *UserManager) LoginUserFromTwitter(ctx context.Context, //
+	screenName string, userId string, oauthToken string, oauthSecret string, //
+	remoteAddr string, userAgent string) (string, *User, error) {
+	userObj, err := obj.GetFromUserName(ctx, screenName+"@twitter")
+	if err != nil {
+		return "", nil, ErrorNotFound
+	}
+	//
+	//
+	m := map[string]interface{}{"oauth_token": oauthToken, "oauth_token_secret": oauthSecret, "screen_name ": screenName, "user_id": userId}
+	b, _ := json.Marshal(m)
+	//
+	loginIdObj, err1 := obj.NewAccessToken(ctx, screenName+"@twitter", remoteAddr, userAgent, string(b))
+	if err1 == nil {
+		obj.UpdateMemcache(ctx, loginIdObj)
+	}
+	return loginIdObj.gaeObject.LoginId, userObj, err1
+}
+
+func (obj *UserManager) MakeRandomId() string {
+	var n uint64
+	binary.Read(rand.Reader, binary.LittleEndian, &n)
+	return strconv.FormatUint(n, 36)
+}
