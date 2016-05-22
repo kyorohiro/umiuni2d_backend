79f24d5d36bfb3d73186e28773748513e13ca875
kyorohiro <kyorohiro@gmail.com>
2016-05-23 08:03:42 +0900

    ..

--- article/article.go
+++ article/article.go
@@ -1,14 +1,7 @@
 package article
 
 import (
-	"crypto/sha1"
-	"encoding/base64"
-	"encoding/binary"
-	"fmt"
-	"strconv"
 	"time"
-
-	"crypto/rand"
 
 	"golang.org/x/net/context"
 	"google.golang.org/appengine/datastore"
@@ -37,57 +30,58 @@ type ArticleManager struct {
 	kindArticle string
 }
 
-func NewArticleManager(kindArticle string) *ArticleManager {
-	ret := new(ArticleManager)
-	ret.kindArticle = kindArticle
-	return ret
+func (obj *ArticleManager) newCursorFromSrc(cursorSrc string) *datastore.Cursor {
+	c1, e := datastore.DecodeCursor(cursorSrc)
+	if e != nil {
+		return nil
+	} else {
+		return &c1
+	}
 }
 
-func (obj *ArticleManager) NewArticle(ctx context.Context, userName string, parentId string) *Article {
-	created := time.Now()
-	var secretKey string
-	var artKey string
-	var key *datastore.Key
-	var art GaeObjectArticle
-	for {
-		secretKey = obj.MakeRandomId()
-		artKey = obj.makeArticleKey(userName, parentId, created, secretKey)
-		key = obj.NewGaeObjectKey(ctx, artKey)
-		err := datastore.Get(ctx, key, &art)
-		if err == nil {
+func (obj *ArticleManager) makeCursorSrc(founds *datastore.Iterator) string {
+	c, e := founds.Cursor()
+	if e == nil {
+		return c.String()
+	} else {
+		return ""
+	}
+}
+
+func (obj *ArticleManager) GetArticleFromUserName(ctx context.Context, userName string, cursorSrc string) ([]*Article, string, string) {
+	q := datastore.NewQuery(obj.kindArticle).Filter("UserName =", userName).Limit(20)
+	return obj.GetArticleFromQuery(ctx, q, cursorSrc)
+}
+
+func (obj *ArticleManager) GetArticleWithNewOrder(ctx context.Context, userName string, cursorSrc string) ([]*Article, string, string) {
+	q := datastore.NewQuery(obj.kindArticle).Filter("State =", "public").Order("-Updated").Limit(20)
+	return obj.GetArticleFromQuery(ctx, q, cursorSrc)
+}
+
+func (obj *ArticleManager) GetArticleFromQuery(ctx context.Context, q *datastore.Query, cursorSrc string) ([]*Article, string, string) {
+	cursor := obj.newCursorFromSrc(cursorSrc)
+	if cursor != nil {
+		q = q.Start(*cursor)
+	}
+	founds := q.Run(ctx)
+
+	var retUser []*Article
+
+	var cursorNext string = ""
+	var cursorOne string = ""
+
+	for i := 0; ; i++ {
+		var d GaeObjectArticle
+		key, err := founds.Next(&d)
+		if err != nil || err == datastore.Done {
 			break
+		} else {
+			retUser = append(retUser, obj.NewArticleFromGaeObject(ctx, key, &d))
+		}
+		if i == 0 {
+			cursorOne = obj.makeCursorSrc(founds)
 		}
 	}
-	//
-	ret := new(Article)
-	ret.gaeObject = &art
-	ret.gaeObjectKey = key
-	ret.gaeObject.UserName = userName
-	ret.gaeObject.ParentId = parentId
-	ret.gaeObject.Created = created
-	ret.gaeObject.Updated = created
-	//
-	return ret
-}
-
-func (obj *ArticleManager) NewGaeObjectKey(ctx context.Context, articleId string) *datastore.Key {
-	return datastore.NewKey(ctx, obj.kindArticle, articleId, 0, nil)
-}
-
-func (obj *ArticleManager) makeArticleKey(userName string, parentId string, created time.Time, secretKey string) string {
-	hashKey := obj.hash(fmt.Sprintf("v1e%s%s%s%s%d", secretKey, userName, userName, parentId, created.UnixNano()))
-	userName64 := base64.StdEncoding.EncodeToString([]byte(userName))
-	return "v1e" + hashKey + parentId + userName64
-}
-
-func (obj *ArticleManager) hash(v string) string {
-	sha1Obj := sha1.New()
-	sha1Obj.Write([]byte(v))
-	return string(sha1Obj.Sum(nil))
-}
-
-func (obj *ArticleManager) MakeRandomId() string {
-	var n uint64
-	binary.Read(rand.Reader, binary.LittleEndian, &n)
-	return strconv.FormatUint(n, 36)
+	cursorNext = obj.makeCursorSrc(founds)
+	return retUser, cursorOne, cursorNext
 }
---
+++ article/manager.go
@@ -0,0 +1,78 @@
+package article
+
+import (
+	"crypto/sha1"
+	"encoding/base64"
+	"encoding/binary"
+	"fmt"
+	"strconv"
+	"time"
+
+	"crypto/rand"
+
+	"golang.org/x/net/context"
+	"google.golang.org/appengine/datastore"
+)
+
+func NewArticleManager(kindArticle string) *ArticleManager {
+	ret := new(ArticleManager)
+	ret.kindArticle = kindArticle
+	return ret
+}
+
+func (obj *ArticleManager) NewArticleFromGaeObject(ctx context.Context, gaeKey *datastore.Key, gaeObj *GaeObjectArticle) *Article {
+	ret := new(Article)
+	ret.gaeObject = gaeObj
+	ret.gaeObjectKey = gaeKey
+	return ret
+}
+
+func (obj *ArticleManager) NewArticle(ctx context.Context, userName string, parentId string) *Article {
+	created := time.Now()
+	var secretKey string
+	var artKey string
+	var key *datastore.Key
+	var art GaeObjectArticle
+	for {
+		secretKey = obj.makeRandomId() + obj.makeRandomId()
+		artKey = obj.makeArticleKey(userName, parentId, created, secretKey)
+		key = obj.NewGaeObjectKey(ctx, artKey)
+		err := datastore.Get(ctx, key, &art)
+		if err == nil {
+			break
+		}
+	}
+	//
+	ret := new(Article)
+	ret.gaeObject = &art
+	ret.gaeObjectKey = key
+	ret.gaeObject.UserName = userName
+	ret.gaeObject.ParentId = parentId
+	ret.gaeObject.Created = created
+	ret.gaeObject.Updated = created
+	ret.gaeObject.ArticleId = artKey
+	//
+	return ret
+}
+
+func (obj *ArticleManager) NewGaeObjectKey(ctx context.Context, articleId string) *datastore.Key {
+	return datastore.NewKey(ctx, obj.kindArticle, articleId, 0, nil)
+}
+
+func (obj *ArticleManager) makeArticleKey(userName string, parentId string, created time.Time, secretKey string) string {
+	hashKey := obj.hash(fmt.Sprintf("v1e%s%s%s%s%d", secretKey, userName, userName, parentId, created.UnixNano()))
+	userName64 := base64.StdEncoding.EncodeToString([]byte(userName))
+	return "v1e" + hashKey + parentId + userName64
+}
+
+func (obj *ArticleManager) hash(v string) string {
+	sha1Obj := sha1.New()
+	sha1Obj.Write([]byte(v))
+	return string(sha1Obj.Sum(nil))
+}
+
+func (obj *ArticleManager) makeRandomId() string {
+	var n uint64
+	binary.Read(rand.Reader, binary.LittleEndian, &n)
+	return strconv.FormatUint(n, 36)
+}
