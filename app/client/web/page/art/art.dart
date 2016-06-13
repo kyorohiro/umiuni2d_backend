ac82d839069065ac740d9b2c02b2dd8ff586ea17
kyorohiro <kyorohiro@gmail.com>
2016-06-14 05:50:37 +0900

    ..

---
+++ app/client/web/page/art/art.dart
@@ -0,0 +1,49 @@
+import 'dart:html' as html;
+import 'dart:async';
+import '../../netbox/netbox.dart' as nbox;
+import '../../netbox/netboxme.dart' as nbox;
+import '../../netbox/netboxfile.dart' as nbox;
+import '../../netbox/netboxart.dart' as nbox;
+import '../../netbox/status.dart' as nbox;
+import '../../dialog/dialog_post.dart' as dialog;
+import '../../util/textbuilder.dart' as util;
+
+class ArtPage {
+  String rootId;
+  String naviId;
+  String iconId;
+  nbox.MyStatus status;
+  nbox.NetBox netbox;
+
+  ArtPage(this.status, this.netbox, this.rootId, {this.naviId: "bbnaviId", this.iconId: "bbiconId"}) {
+    html.window.onHashChange.listen((_) {
+      updateFromHash();
+    });
+    init();
+  }
+
+  Future updateFromHash() async {
+    if (this.status.isLogin == false) {
+      return;
+    }
+    String hash = html.window.location.hash;
+    Map<String,String> prop = {};
+    if (hash.indexOf("?") > 0) {
+      prop = Uri.splitQueryString(hash.substring(hash.indexOf("?") + 1));
+      hash = hash.substring(0, hash.indexOf("?"));
+    }
+    if (hash.startsWith("#/Article")) {
+      if (hash == "#/Article/get") {
+        if (prop[nbox.NetBox.ReqPropertyArticleId] != null) {
+          update(prop[nbox.NetBox.ReqPropertyArticleId]);
+        }
+      }
+    }
+  }
+
+  init(){
+  }
+
+  update(String articleId) async {
+  }
+}
