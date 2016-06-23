ee68cea8b30b8bc92b1792d36c837726d4b2884e
kyorohiro <kyorohiro@gmail.com>
2016-06-23 21:30:24 +0900

    ..

--- app/client/web/netbox/netboxfeed.dart
+++ app/client/web/netbox/netboxfeed.dart
@@ -3,6 +3,7 @@ import "netbox.dart" as netbox;
 import "dart:async";
 
 typedef Future<netbox.NetBoxArtManagerFind> FuncfindArticle(String cursor);
+
 class NetBoxFeed {
   String headCursor = "";
   String tailCursor = "";
@@ -11,33 +12,46 @@ class NetBoxFeed {
   String apiKey;
   String version;
   String passwordKey;
-  FuncfindArticle  funcfindArticle;
+  FuncfindArticle funcfindArticle;
 
   List<netbox.NetBoxArtManagerFindArt> founded = [];
-  Map<String,netbox.NetBoxArtManagerFindArt> foundedHash = {};
+  Map<String, netbox.NetBoxArtManagerFindArt> foundedHash = {};
 
-  NetBoxFeed(this.backendAddr, this.apiKey, {this.version: "v1", this.passwordKey: "umiuni2d", //
-    this.funcfindArticle:null}) {
-      if(this.funcfindArticle == null) {
-          netbox.NetBoxArtManager art = new netbox.NetBoxArtManager(backendAddr, apiKey);
-        this.funcfindArticle = art.findArticleWithNewOrde;
-      }
+  NetBoxFeed(this.backendAddr, this.apiKey,
+      {this.version: "v1",
+      this.passwordKey: "umiuni2d", //
+      this.funcfindArticle: null}) {
+    if (this.funcfindArticle == null) {
+      netbox.NetBoxArtManager art = new netbox.NetBoxArtManager(backendAddr, apiKey, version: version, passwordKey: passwordKey);
+      this.funcfindArticle = art.findArticleWithNewOrde;
     }
+  }
 
+  factory NetBoxFeed.username(String userName, String backendAddr, String apiKey, //
+      {String version: "v1",
+      String passwordKey: "umiuni2d"}) {
+    Future<netbox.NetBoxArtManagerFind> adapter(String cursor) {
+      netbox.NetBoxArtManager art = new netbox.NetBoxArtManager(backendAddr, apiKey, version: version, passwordKey: passwordKey);
+      return art.findArticleWithUserName(userName, cursor);
+    }
+    ;
+    return new NetBoxFeed(backendAddr, apiKey,
+        version: version,
+        passwordKey: passwordKey, //
+        funcfindArticle: adapter);
+  }
 
   Future<List<netbox.NetBoxArtManagerFindArt>> next() async {
-    netbox.NetBoxArtManagerFind a  = await this.funcfindArticle(this.tailCursor);
-    if(a.code == netbox.NetBox.ReqPropertyCodeOK) {
+    netbox.NetBoxArtManagerFind a = await this.funcfindArticle(this.tailCursor);
+    if (a.code == netbox.NetBox.ReqPropertyCodeOK) {
       tailCursor = a.cursorNext;
     }
     for (var v in a.arts) {
-      if( false == foundedHash.containsKey(v.articleId)) {
+      if (false == foundedHash.containsKey(v.articleId)) {
         foundedHash[v.articleId] = v;
         founded.add(v);
       }
     }
     return a.arts;
   }
-
-
 }
