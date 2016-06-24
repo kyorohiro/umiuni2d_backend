69682a3965a7b9932e46135bb68c862362cb64cf
kyorohiro <kyorohiro@gmail.com>
2016-06-24 16:12:11 +0900

    ..

--- app/client/web/page/feed/feed.dart
+++ app/client/web/page/feed/feed.dart
@@ -80,27 +80,26 @@ class FeedPage {
 
     var ticket = builder.pat(builder.getRootTicket(), [
       """<nav class="${this.naviId}">""", //
-      """		<ul id="${this.feedContainerId}">""",],[
-      """		</ul>""",
-      """</nav> """,
+      """		<ul id="${this.feedContainerId}">""",
+      """		</ul>""",],
+      ["""</nav> """,
     ]);
-
-    /*
-    for (var v in feeder.founded) {
-      builder.end(ticket, [
-        """    <li><a href="#/Article/get?${nbox.NetBox.ReqPropertyArticleId}=${Uri.encodeComponent(v.articleId)}"><div style="width:${w}px;">""",
-        """      <table><tr><td> """,
-        """       <img id="${this.iconId}" style="width:50px;display:inline; background-color:#99cc00;" src="${netbox.newMeManager().makeImgUserIconSrc(v.userName)}">""", //
-        """      </td><td>""", ////
-        """       <div style="font-size:15px"> ${v.title} """,
-        """         <div style="font-size:10px"> ${v.userName} ${v.updated}</div>""",
-        """       </div><br>""",
-        """      </td></tr></table>""",
-        """      <div style="font-size:10px"> ${v.tag} </div>""",
-        """      <div style="font-size:8px">${v.articleInfo}</div>""",
-        """      </div></a></li>""",]);
+    int w = 250;
+    if(w > html.window.innerWidth) {
+      w = html.window.innerWidth;
     }
-*/
+    builder.end(ticket, [
+      """    <ul><li><a><div style="width:${w}px;">""",
+      """      <table><tr><td> """,
+//      """       <img id="${this.iconId}" style="width:50px;display:inline; background-color:#99cc00;" src="${netbox.newMeManager().makeImgUserIconSrc(v.userName)}">""", //
+      """      </td><td>""", ////
+      """       <div style="font-size:15px"> Next """,
+      """         <div style="font-size:10px"> </div>""",
+      """       </div><br>""",
+      """      </td></tr></table>""",
+      """      <div style="font-size:10px">  </div>""",
+      """      <div style="font-size:8px"></div>""",
+      """      </div></a></li></ul>""",]);
 
     elm.appendHtml(builder.toText("\r\n"), treeSanitizer: html.NodeTreeSanitizer.trusted);
     nextFeed(isInit:true);
