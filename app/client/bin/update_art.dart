3e430375c3efb950da30ca4e0adee3ae9ab63154
kyorohiro <kyorohiro@gmail.com>
2016-07-22 18:05:20 +0900

    ..

--- app/client/bin/update_art.dart
+++ app/client/bin/update_art.dart
@@ -1,4 +1,14 @@
 
+import "dart:io" as io;
+import "package:csv/csv.dart" as csv;
+import "dart:convert" as convert;
+
 void main(List<String> args) {
   print(">>>> ${args}");
+  io.File f = new io.File("${args[0]}");
+  var v = f.readAsBytesSync();
+
+  csv.CsvCodec c = new csv.CsvCodec(fieldDelimiter: ",");
+  var w = c.decode(convert.UTF8.decode(v));
+  print(w);
 }
