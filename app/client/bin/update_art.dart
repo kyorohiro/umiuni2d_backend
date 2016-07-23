19cec55790d4bedb466417fdfb5eed9339700384
kyorohiro <kyorohiro@gmail.com>
2016-07-23 17:22:29 +0900

    ..

--- app/client/bin/update_art.dart
+++ app/client/bin/update_art.dart
@@ -2,29 +2,34 @@
 import "dart:io" as io;
 import "package:csv/csv.dart" as csv;
 import "dart:convert" as convert;
+import 'package:tetorica/net.dart' as tet;
+import 'package:tetorica/net_dartio.dart' as tet;
+
 import 'package:umiuni2d_backend_client/nbox.dart' as nbox;
 import 'package:umiuni2d_backend_client/netboxdartio.dart' as nbox;
 void main(List<String> args) {
   print(">>>> ${args}");
-  io.File f = new io.File("${args[0]}");
-  var v = f.readAsBytesSync();
   //
   // login from twitter
   nbox.NetBox rootBox = new nbox.NetBox(//
-    new nbox.TinyNetDartIoBuilder(), "http://127.0.0.1:8080", "A91A3E1B-15F0-4DEE-8ECE-F5DD1A06230E");
+    new nbox.TinyNetTetoricaBuilder(new tet.TetSocketBuilderDartIO()), "http://127.0.0.1:8080", "A91A3E1B-15F0-4DEE-8ECE-F5DD1A06230E");
 
   io.HttpServer.bind("0.0.0.0", 8086).then((io.HttpServer server) {
     server.listen((io.HttpRequest request) {
       print(request.uri.path);
       print(request.uri.queryParameters);
     });
-    
+
     rootBox.newMeManager().loginWithTwitter("http://127.0.0.1:8086").then(//
       (nbox.NetBoxMeManagerLoginTwitter res){
         print("## ${res.code} ${res.url}");
     //    runBrowser("${res.url}");
     });
   });
+
+  //  io.File f = new io.File("${args[0]}");
+  //  var v = f.readAsBytesSync();
+
   //
   //
   //csv.CsvCodec c = new csv.CsvCodec(fieldDelimiter: ",");
--- app/client/lib/netbox/dartiorequester.dart
+++ app/client/lib/netbox/dartiorequester.dart
@@ -1,4 +1,68 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:typed_data';
+//import 'package:'
+import 'package:tetorica/net.dart' as tet;
+import 'package:tetorica/http.dart' as tet;
 import 'requester.dart';
+
+class TinyNetTetoricaBuilder extends TinyNetBuilder {
+  tet.TetSocketBuilder builder;
+  TinyNetTetoricaBuilder(this.builder) {}
+
+  Future<TinyNetRequester> createRequester() async {
+    return new TinyNetTetoricaHttpRequester(builder);
+  }
+}
+
+class TinyNetTetoricaHttpRequester extends TinyNetRequester {
+  tet.TetSocketBuilder builder;
+  TinyNetTetoricaHttpRequester(this.builder) {}
+
+  Future<TinyNetRequesterResponse> request(String type, String url, {Object data: null, Map<String, String> headers: null}) async {
+    if (headers == null) {
+      headers = {};
+    }
+    tet.HttpClientHelper cl = new tet.HttpClientHelper(builder);
+
+    tet.HttpClientResponse res = null;
+    Uri uri = Uri.parse(url);
+    List<int> dat = const [];
+    if (data is List<int>) {
+      dat = data;
+    } else if (data is ByteBuffer) {
+      dat = data.asUint8List();
+    } else if (data is ByteData) {
+      dat = data.buffer.asUint8List();
+    } else if (data is String) {
+      dat = UTF8.encode(data);
+    }
+
+    if (type.toUpperCase() == TinyNetRequester.TYPE_POST) {
+      res = await cl.post(uri.host, uri.port, "${uri.path}?${uri.query}", dat, header: headers, useSecure: (uri.scheme == "https"));
+    } else if (type.toUpperCase() == TinyNetRequester.TYPE_GET) {
+      res = await cl.get(uri.host, uri.port, "${uri.path}?${uri.query}", header: headers, useSecure: (uri.scheme == "https"));
+    } else if (type.toUpperCase() == TinyNetRequester.TYPE_PUT) {
+      res = await cl.put(uri.host, uri.port, "${uri.path}?${uri.query}", dat, header: headers, useSecure: (uri.scheme == "https"));
+    } else if (type.toUpperCase() == TinyNetRequester.TYPE_DELETE) {
+      res = await cl.delete(uri.host, uri.port, "${uri.path}?${uri.query}", header: headers, useSecure: (uri.scheme == "https"));
+    } else {
+      throw new UnsupportedError("");
+    }
+
+    Map<String, String> retHeader = {};
+    for (tet.HttpResponseHeaderField h in res.message.headerField) {
+      retHeader[h.fieldName] = h.fieldValue;
+    }
+    Uint8List v = new Uint8List.fromList(await res.body.getAllBytes());
+    return new TinyNetRequesterResponse(res.message.line.statusCode, retHeader, v.buffer);
+  }
+
+  Future<String> srcToMultipartData(String src) async {
+    return "";
+  }
+}
+/*import 'requester.dart';
 import 'dart:async';
 import 'dart:io' as io;
 import 'dart:typed_data';
@@ -57,3 +121,4 @@ class TinyNetDartIoHttpRequester extends TinyNetRequester {
     return "";
   }
 }
+*/
